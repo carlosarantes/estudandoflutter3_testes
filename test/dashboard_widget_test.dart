123a68759447356e47c3116acfcf389e9c91482c
Carlos Arantes <carlos@ipe.digital>
2020-03-16 22:52:15 -0300

    estudando testes

--- test/dashboard_widget_test.dart
+++ test/dashboard_widget_test.dart
@@ -18,11 +18,45 @@ void main() {
 
   testWidgets('Should display the transfer feature when the Dashboard is opened.', (tester) async {
     await tester.pumpWidget(MaterialApp(home: Dashboard(), ));
+    /*
     final iconTransfferFeatureItem = find.widgetWithIcon(FeatureItem, Icons.add);
     expect(iconTransfferFeatureItem, findsOneWidget);
 
     final nameTransferFeatureItem = find.widgetWithText(FeatureItem, 'Transfer');
     expect(nameTransferFeatureItem, findsOneWidget);
+    */
+
+    final transferFeatureItem = find.byWidgetPredicate((widget) {
+      return featureItemMatcher(widget, 'Transfer', Icons.monetization_on);
+    });
+
+    expect(transferFeatureItem, findsOneWidget);
 
   });
+
+  testWidgets('Should display the transaction feed feature when the Dashboard is opened.', (tester) async {
+    await tester.pumpWidget(MaterialApp(home: Dashboard(), ));
+    
+    final transactionFeedFeatureItem = find.byWidgetPredicate((widget) {
+      return featureItemMatcher(widget, 'Transaction Feed', Icons.description);
+    });
+    expect(transactionFeedFeatureItem, findsOneWidget);
+   
+    /*
+    final iconTransactionFeedFeatureItem = find.widgetWithIcon(FeatureItem, Icons.description);
+    expect(iconTransactionFeedFeatureItem, findsOneWidget);
+
+    final nameTransactionFeedFeatureItem = find.widgetWithText(FeatureItem, 'Transaction Feed');
+    expect(nameTransactionFeedFeatureItem, findsOneWidget);
+    */
+
+  });
+
+}
+
+bool featureItemMatcher(Widget widget, String name, IconData icon) {
+  if(widget is FeatureItem) {
+      return widget.name == name && widget.icon == icon;
+  }
+  return false;
 }
