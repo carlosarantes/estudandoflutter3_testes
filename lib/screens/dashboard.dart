88031df9cdd62d9c4ca55eb3bd6c539822833ed5
Carlos Arantes <carlos@ipe.digital>
2020-03-16 22:26:16 -0300

    estudando testes

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -9,38 +9,58 @@ class Dashboard extends StatelessWidget {
 
     return Scaffold(
         appBar: AppBar(title: Text('Dashboard'),),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Image.asset('images/bytebank_logo.png'),
-            ),
-            Container(
-              height: 120,
-              child: ListView(
-                      scrollDirection: Axis.horizontal,
-                      children: <Widget>[
-                          FeatureItem(
-                                        'Transfer', 
-                                        Icons.monetization_on,
-                                        onClick : (){
-                                          _showContactsList(context);
-                                        }, ),
-                          FeatureItem(
-                                        'Transaction Feed', 
-                                        Icons.description, 
-                                        onClick : (){
-                                          _showTransactionsList(context);
-                                        },
-                          ),
-                      ],
-                    ), 
-            )
+        body: 
+              LayoutBuilder(
+                builder: (context, constraints) {
+                  return SingleChildScrollView(
+                            child: ConstrainedBox(
+                                    constraints: BoxConstraints(
+                                        minHeight: constraints.maxHeight
+                                    ),
+                                    child: Column(
+                                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                      children: <Widget>[
+                                        Padding(
+                                          padding: const EdgeInsets.all(8.0),
+                                          child: Image.asset('images/bytebank_logo.png'),
+                                        ),
+                                        Container(
+                                          height: 120,
+                                          child: ListView(
+                                                  scrollDirection: Axis.horizontal,
+                                                  children: <Widget>[
+                                                      FeatureItem(
+                                                                    'Transfer', 
+                                                                    Icons.monetization_on,
+                                                                    onClick : (){
+                                                                      _showContactsList(context);
+                                                                    }, ),
+                                                      FeatureItem(
+                                                                    'Transaction Feed', 
+                                                                    Icons.description, 
+                                                                    onClick : (){
+                                                                      _showTransactionsList(context);
+                                                                    },
+                                                      ),
+                                                  ],
+                                                ), 
+                                        )
 
-          ],
-        ), 
+                                      ],
+                                    ), 
+                            ),
+                            
+                            
+                                  
+
+
+                              );
+
+                },  
+              ),
+      
+
       );
   }
 
